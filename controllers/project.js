174fcb2599e43c1e9a6aa6f49ed335fb3aaa778a
Miha <miha.fink@signapps.si>
2019-08-22 13:29:28 +0200

    Updated project controller.

--- controllers/project.js
+++ controllers/project.js
@@ -9,25 +9,28 @@ const Project = require('../models/project');
  *
  */
 async function index(ctx, next) {
-	let projects = await Project.get();
+	let projects = await Project.whereNotIn('name', ["Vacation", "Sick leave", "Extra hours"]).get();
 	ctx.body = projects;
 }
 
 /**
- * Returns a list of all projects.
+ * Store project.
  *
  * @param {string} name Project name.
  * @param {string} tag Project tag can be between 1 and 10 characters.
+ * @param {string} [color] Project color hex code.
  */
 async function store(ctx, next) {
 	let body = Joi.validate(ctx.request.body, Joi.object().keys({
 		name: Joi.string().required(),
 		tag: Joi.string().min(1).max(10).required(),
+		color: Joi.string().default(null),
 	}));
 	// Create the project.
 	let project = new Project({
 		name: body.name,
 		tag: body.tag.toUpperCase(),
+		color: body.color,
 	});
 
 	// Save the new project.
@@ -41,14 +44,16 @@ async function store(ctx, next) {
  * @param {integer} id Project id.
  * @param {string} [name] Project name.
  * @param {string} [tag] Project tag can be between 1 and 10 characters.
+ * @param {string} [color] Project color hex code.
  */
 async function update(ctx, next) {
 	let id = parseInt(ctx.params.id);
 	let body = Joi.validate(ctx.request.body, Joi.object().keys({
 		name: Joi.string().default(null),
 		tag: Joi.string().min(1).max(10).default(null),
+		color: Joi.string().default(null),
 	}));
-	let project = await Project.select(['id', 'name', 'tag']).where('id', id).first();
+	let project = await Project.select(['id', 'name', 'tag', 'color']).where('id', id).first();
 
 	// Check if the project was found.
 	ctx.assert(project, 400, ctx.i18n.__(`Project with id ${id} not found.`));
@@ -59,6 +64,7 @@ async function update(ctx, next) {
 		let tag = body.tag.toUpperCase();
 		project.set('tag', tag);
 	}
+	if (body.color !== null) project.set('color', body.color);
 
 	let name = project.toJSON().name;
 	let tag = project.toJSON().tag;
