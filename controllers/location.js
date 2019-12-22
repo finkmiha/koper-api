c946506a1b4dc47281935f3141e4c0f0babdb25a
Nejc Fink <nejcfink1@gmail.com>
2019-12-22 14:57:33 +0100

    Location api updated with difficulty level.

--- controllers/location.js
+++ controllers/location.js
@@ -34,9 +34,17 @@ async function getLocation(ctx, next) {
 
     delete location.created_at;
     delete location.updated_at;
+
+    // Add Difficulty level
+    for(var i = 0; i<locationRoutes.length; i++){
+    let difficulty_level_indetifier = locationRoutes[i].difficulty.split('(')[0].trim();
+    var difficulty_level = aDifficulties.indexOf(difficulty_level_indetifier);
+    locationRoutes[i]['difficulty_level'] = difficulty_level;
+    }
+    // Add Difficulty level END
+
     location = Object.assign(location, {routes: locationRoutes});
     
-
 	ctx.body = location;
 }
 
@@ -48,3 +56,75 @@ module.exports = {
     index,
     getLocation,
 };
+
+
+let aDifficulties = [
+"2",
+"3",
+"4a",
+"4a+",
+"4a+/b",
+"4a/a+",
+"4b",
+"4b+",
+"4b+/c",
+"4b/b+",
+"4c",
+"4c+",
+"4c+/5a",
+"4c/c+",
+"5a",
+"5a+",
+"5a+/b",
+"5a,A0",
+"5a/a+",
+"5b",
+"5b+",
+"5b+/c",
+"5b,A2",
+"5b/b+",
+"5c",
+"5c+",
+"5c+/6a",
+"5c/c+",
+"6a",
+"6a+",
+"6a+/b",
+"6a,A2",
+"6a/a+",
+"6b",
+"6b+",
+"6b+/c",
+"6b/b+",
+"6c",
+"6c+",
+"6c+/7a",
+"6c/c+",
+"7a",
+"7a+",
+"7a+/b",
+"7a/a+",
+"7b",
+"7b+",
+"7b+/c",
+"7b/b+",
+"7c",
+"7c+",
+"7c+/8a",
+"7c/c+",
+"8a",
+"8a+",
+"8a+/b",
+"8a/a+",
+"8b",
+"8b+",
+"8b+/c",
+"8b/b+",
+"8c",
+"8c+",
+"8c+/9a",
+"8c/c+",
+"9a",
+"9a+/b",
+"9a/a+",
+]
