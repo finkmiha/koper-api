ded681e364e4daf22f12aff99d790ccda50be5ca
Nejc Fink <nejcfink1@gmail.com>
2019-12-22 17:05:47 +0100

    Nadaljuj v scripts/generate_coordinates
    Use Generate coordinates for updating locations coordinates.

--- scripts/generate_coordinates.js
+++ scripts/generate_coordinates.js
@@ -12,27 +12,34 @@ let options = {
 let geocoder = NodeGeocoder(options);
 
 async function getLocationCoordinates() {
-    let locations = await Location.select("name").get();
+    let locations = await Location.select(["id","name"]).get();
     locations = locations.toJSON();
     let coords = [];
 
-    for (let location of locations) {
+    for (let [i, location] of locations.entries()) {
         let res = await geocoder.geocode(location.name);
 
+        let geoLoopMatch = false
         for(let i = 0; i < res.length; i++){
             if(res[i].countryCode === "SI" && res[i].formattedAddress.includes(location.name)){
-                let latitude = res[i].latitude || null;
-                let longitude = res[i].longitude || null;
-
+                let latitude = res[i].latitude;
+                let longitude = res[i].longitude;
                 coords.push([longitude, latitude]);
+                geoLoopMatch = true
             }
+        } if(geoLoopMatch === false){
+            let latitude = null;
+            let longitude = null;
+            coords.push([longitude, latitude]);
         }
+
+        //Insert into DB
+        console.log(location.id +" "+ location.name + " coordinates are = " + coords[i])
+        //await Location.update({lat: latitude, lng: longitude }).where("id",location.id);
+        //Insert into DB end
     }
-    console.log(coords);
-    console.log(coords.length);
-    console.log("Done");
 }
+    
+
 
 getLocationCoordinates();
-
-
